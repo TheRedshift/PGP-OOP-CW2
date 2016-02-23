43682ebc5a48b48c08944b1bd6258bb91a4d0d2a
psyrs11@nottingham.ac.uk <psyrs11@nottingham.ac.uk>
2016-02-23 08:43:09 +0000

    imports

---
+++ src/CW2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Rahul Soni on 23/02/2016.
+ */
+
+
+public class CW2 {
+
+
+        public static void main (String[] args)
+        {
+            JFrame frame = new JFrame();
+            frame.add( new ColorLabel(300,100, Color.PINK,5,Color.BLUE) );
+
+
+        }
+}
